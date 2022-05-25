d0bfacbb173e15aaa2ea1e0b705839197c3adeb4
Axtrean <103894317+Axtrean@users.noreply.github.com>
2022-05-25 22:30:23 +0300

    ё

--- js/alert.js
+++ js/alert.js
@@ -9,3 +9,41 @@ sitedes);
 
 let adapt = prompt("Выберите адаптивность сайта. 1 - Под мобильные телефоны, 2 - под компьютеры, 3 -Под всё");
 console.log("adapt " + adapt);
+
+let num = sitetype + sitedes + adapt;
+
+console.log(num);
+
+let price = 0;
+
+if (sitetype == 1){
+    price += 2000
+}
+if (sitetype == 2){
+    price += 2200
+}
+if (sitetype == 3){
+    price += 20000
+}
+if (sitedes == 1){
+    price += 1000
+}
+if (sitedes == 2){
+    price += 2000
+}
+if (sitedes == 3){
+    price += 10000
+}
+if (adapt == 1){
+    price += 1000
+}
+if (adapt == 2){
+    price += 3000
+}
+if (adapt == 3){
+    price += 10000
+}
+
+console.log(price);
+
+alert(price + " рублей");
