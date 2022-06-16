45c32ad3104218c5da0d8040ec858315b580336f
Axtrean <103894317+Axtrean@users.noreply.github.com>
2022-06-16 22:53:51 +0300

    Добавление калькулятора и сроков

--- js/alert.js
+++ js/alert.js
@@ -7,10 +7,18 @@ function calc(){
     var result = document.getElementById("result");
     
     var timecode = 0;
-    timecode += parseInt(hsitetype.options[hsitetype.selectedIndex].value1);
-    parseInt(hadapt.options[hadapt.selectedIndex].value1);
-    parseInt(hsitedes.options[hsitedes.selectedIndex].value1);
-    timeresult.innerHTML = timecode;
+    timecode += parseInt(hsitetype.options[hsitetype.selectedIndex].getAttribute('value1'));
+    timecode += parseInt(hadapt.options[hadapt.selectedIndex].getAttribute('value1'));
+    timecode += parseInt(hsitedes.options[hsitedes.selectedIndex].getAttribute('value1'));
+    if (timecode == 1){
+    timeresult.innerHTML = timecode + " день";
+    };
+    if (timecode >= 2 && timecode <= 4){
+        timeresult.innerHTML = timecode + " дня";
+    };
+    if (timecode >= 5){
+        timeresult.innerHTML = timecode + " дней";
+    };
     
     var price = 0;
     price += parseInt(hsitetype.options[hsitetype.selectedIndex].value);
